dde05bc25f3026dd028c8e150952777c5f9390ec
Enzo Giacomini <203458225+EnzoGiacomini@users.noreply.github.com>
2026-01-27 17:11:30 -0300

    Multiplayer local finished

---
+++ tic-tac-toe/game.h
@@ -0,0 +1,28 @@
+#ifndef GAME_H
+#define GAME_H
+
+typedef struct {
+    char table[9];
+    char player_name[21];
+    int multiplayer;
+    char player2_name[21];
+    int turn;
+    int play;
+    int gamestate;
+    int n_plays;
+    int endGame;
+} Game;
+
+void bufferClear();
+void clear_table(Game **g);
+void start(Game **g);
+void get_user_name(Game **g);
+void sort_first_player(Game **g);
+void show_player_turn(Game **g);
+void tictactoe(Game **g);
+void show_table(Game **g);
+void tutorial();
+void insertPlay(Game **g);
+void reset_game(Game **g);
+
+#endif
--- tic-tac-toe/tic-tac-toe.c
+++ tic-tac-toe/tic-tac-toe.c
@@ -1,11 +1,31 @@
-#include <stdio.h>
+#include "game.h"
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 
-int main ()
-{
-    
+int main() {
+
+    Game *game = (Game *)calloc(1,sizeof(Game));
+    if (!game) return 1;
+
+    do
+    {
+
+        reset_game(&game);
+
+        start(&game);
+        
+        if(game->multiplayer == 1) {
+            get_user_name(&game);
+            sort_first_player(&game);
+            tictactoe(&game);
+        }
+        else
+        {
+
+        }
+
+    } while(game->endGame == 0);
+
+    free(game);
 
     return 0;
 }
